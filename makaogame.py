39a818c6eff24f914379a5e1e25e509b326ec57c
dobryfranek <dobrowolski.franciszek@gmail.com>
2025-11-20 23:54:09 +0100

    helpful commit message

--- makaogame.py
+++ makaogame.py
@@ -3,4 +3,4 @@ class Makao():
         self.card_amount = 52 * num_decks
         self.colors = ["heart", "spade", "diamond", "club"] if use_english else ["kier", "pik", "karo", "trefl"]
         self.values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "Q", "J", "K"]
-	self.num_players = num_players
+        self.num_players = num_players

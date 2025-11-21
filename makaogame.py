d65bdf095907c9a0e8c1d056d48b1758ec175f96
hacker <a.jakubowska@t1piaseczno.edu.pl>
2025-11-21 13:13:30 +0100

    helpful commit message

--- makaogame.py
+++ makaogame.py
@@ -1,6 +1,44 @@
+from random import shuffle
+
+MAKAO_COLORS = ["kier", "pik", "karo", "trefl"]
+MAKAO_VALUES = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "Q", "J", "K"]
+
+class Card():
+    def __init__(self, color: str, value: str):
+        assert color in MAKAO_COLORS and value in MAKAO_VALUES
+        self.color = color
+        self.value = value
+
+class Player():
+    def __init__(self, name: str = "undefined", hand: list[tuple] = []):
+        self.name = name
+        self.hand = hand
+
+
 class Makao():
-    def __init__(self, num_players: int = 4, num_decks: int = 1, use_english: bool = False):
+    def __init__(self, players: list[Player] = [Player() for _ in range(4)], num_decks: int = 1):
         self.card_amount = 52 * num_decks
-        self.colors = ["heart", "spade", "diamond", "club"] if use_english else ["kier", "pik", "karo", "trefl"]
-        self.values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "Q", "J", "K"]
-        self.num_players = num_players
+
+        self.colors = MAKAO_COLORS
+        self.values = MAKAO_VALUES
+
+        self.num_players = len(players)
+        self.player_hands = {player: [] for player in players} # wywal
+        self.current_turn = 0
+        self.reserve_deck = []
+        self.played_deck = []
+        self.create_deck()
+    
+    def create_deck(self):
+        for deck in range(self.card_amount // 52):
+            for value in self.values:
+                for color in self.colors:
+                    self.reserve_deck.append(Card(color, value))
+        shuffle(self.reserve_deck)
+        shuffle(self.reserve_deck)
+        shuffle(self.reserve_deck)
+        for c in self.reserve_deck:
+            print(c.color, c.value)
+
+if __name__ == "__main__": # for test
+    macau = Makao()

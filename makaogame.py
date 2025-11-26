598eb4ea594903abfc3c6130590870b0432e7d8d
hacker <a.jakubowska@t1piaseczno.edu.pl>
2025-11-26 11:32:42 +0100

    helpful

--- makaogame.py
+++ makaogame.py
@@ -10,20 +10,20 @@ class Card():
         self.value = value
 
 class Player():
-    def __init__(self, name: str = "undefined", hand: list[tuple] = []):
+    def __init__(self, name: str, hand: list[tuple] = []):
         self.name = name
         self.hand = hand
 
 
 class Makao():
-    def __init__(self, players: list[Player] = [Player() for _ in range(4)], num_decks: int = 1):
+    def __init__(self, players: list[str], num_decks: int = 1):
         self.card_amount = 52 * num_decks
 
         self.colors = MAKAO_COLORS
         self.values = MAKAO_VALUES
 
+        self.players = [Player(name) for name in players]
         self.num_players = len(players)
-        self.player_hands = {player: [] for player in players} # wywal
         self.current_turn = 0
         self.reserve_deck = []
         self.played_deck = []
@@ -39,6 +39,19 @@ class Makao():
         shuffle(self.reserve_deck)
         for c in self.reserve_deck:
             print(c.color, c.value)
+    
+    def deal_hands(self, verbose=False, cards: int = 5):
+        shuffle(self.reserve_deck)
+        shuffle(self.reserve_deck)
+        shuffle(self.reserve_deck)
+        assert len(self.reserve_deck) == self.card_amount
+        for i in range(cards):
+            for player in self.players:
+                player.hand.append(self.reserve_deck.pop())
+        if verbose: print(self.players[0].hand)
+            
+        
+
 
 if __name__ == "__main__": # for test
     macau = Makao()
